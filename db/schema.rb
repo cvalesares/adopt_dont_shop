0de659ab85263310b3c9cdcf56d6d1c15a1c81b0
Christian Valesares <cvalesares@gmail.com>
2021-10-24 12:17:07 -0400

    add application show feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_23_171909) do
+ActiveRecord::Schema.define(version: 2021_10_23_192744) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "applications", force: :cascade do |t|
     t.string "name"
+    t.string "address"
+    t.string "description"
+    t.string "status"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "pet_applications", force: :cascade do |t|
-    t.bigint "application_id"
     t.bigint "pet_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.bigint "application_id"
     t.index ["application_id"], name: "index_pet_applications_on_application_id"
     t.index ["pet_id"], name: "index_pet_applications_on_pet_id"
   end
